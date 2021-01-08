d4eedc50a4620ca9e60b2adfb46012f69c126ac5
elitonmarcos <elitonmarcos@gmail.com>
2021-01-08 17:29:18 -0300

    Teste CyberGestion

---
+++ Controllers/telefonesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Contatos.Models;
+
+namespace Contatos.Controllers
+{
+    public class telefonesController : Controller
+    {
+        private contexto db = new contexto();
+
+        // GET: telefones
+        public ActionResult Index()
+        {
+            return View(db.Telefone.ToList());
+        }
+
+        // GET: telefones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            telefone telefone = db.Telefone.Find(id);
+            if (telefone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(telefone);
+        }
+
+        // GET: telefones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: telefones/Create
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Tipo,Numero")] telefone telefone)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Telefone.Add(telefone);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(telefone);
+        }
+
+        // GET: telefones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            telefone telefone = db.Telefone.Find(id);
+            if (telefone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(telefone);
+        }
+
+        // POST: telefones/Edit/5
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Tipo,Numero")] telefone telefone)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(telefone).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(telefone);
+        }
+
+        // GET: telefones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            telefone telefone = db.Telefone.Find(id);
+            if (telefone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(telefone);
+        }
+
+        // POST: telefones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            telefone telefone = db.Telefone.Find(id);
+            db.Telefone.Remove(telefone);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/telefone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Contatos.Models
+{
+    public class telefone
+    {
+        [Key]
+        [DisplayName("Código")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [DisplayName("Nome")]
+        [MaxLength(100), MinLength(3)]
+        public string Nome { get; set; }
+        [DisplayName("Tipo")]
+        [Display(Name = "Tipo")]
+        [Required(ErrorMessage = "Escolha um Tipo de Telefone")]
+        public TipoTelefone Tipo { get; set; }
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [MaxLength(13), MinLength(10)]
+        [DisplayName("Telefone")]
+        public string Numero { get; set; }
+
+        public enum TipoTelefone
+        {
+            [Display(Name = "Celular")]
+            Celular = 1,
+            [Display(Name = "Residencial")]
+            Residencial = 2,
+            [Display(Name = "Trabalho")]
+            Trabalho = 3,
+            [Display(Name = "WhatsApp")]
+            WhatsApp = 4,
+        }
+
+    }
+
+}
